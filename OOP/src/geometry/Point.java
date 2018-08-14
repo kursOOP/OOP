74f6829f7d342776581df10e3d4d89c065944a06
vukmanovic.milos <vukmanovic.milos@gmail.com>
2018-08-14 15:50:23 +0200

    Vežbe 3/4 (zadaci 4-7)

---
+++ OOP/src/geometry/Point.java
@@ -0,0 +1,35 @@
+package geometry;
+
+public class Point {
+	private int x;
+	private int y;
+	private boolean selected;
+
+	public double distance(Point p) {
+		int dX = x - p.x;
+		int dY = p.y - y;
+		double d = Math.sqrt(dX*dX + dY*dY);
+		return d;
+	}
+
+	public int getX() {
+		return x;
+	}
+	public void setX(int x) {
+		this.x = x;
+	}
+	public int getY() {
+		return y;
+	}
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	public boolean isSelected() {
+		return selected;
+	}
+
+	public void setSelected(boolean selected) {
+		this.selected = selected;
+	}
+}
